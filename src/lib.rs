0c0b28635258548c48353d334aa93a1e6116559f
Robert <robert.balicki@gmail.com>
2018-07-15 03:04:45 -0400

    went back to nothing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(wasm_custom_section, wasm_import_module, proc_macro)]
+#![feature(wasm_custom_section, wasm_import_module, proc_macro, nll)]
 
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
@@ -12,21 +12,20 @@ pub mod js_fns;
 
 extern crate serde_json;
 
-thread_local! {
-  static ROOT_COMPONENT: RefCell<Option<Box<Component>>> = RefCell::new(None);
-}
+// thread_local! {
+//   static ROOT_COMPONENT: RefCell<Option<Box<Component<'static>>>> = RefCell::new(None);
+// }
 
-pub fn mount(div_id: &str, mut component: Box<dyn Component + 'static>) {
-  js_fns::initialize(div_id, Interface {});
+pub fn mount(div_id: &str, mut component: Box<dyn Component>) {
+  // js_fns::initialize(div_id, Interface {}, component);
 
-  ROOT_COMPONENT.with(|rc| {
-    let token = component.render();
+  //   let token = component.render();
 
-    js_fns::render(&token.as_inner_html());
-    js_fns::log(&token.as_inner_html());
+  //   js_fns::render(&token.as_inner_html());
+  //   js_fns::log(&token.as_inner_html());
 
-    *rc.borrow_mut() = Some(component);
-  });
+  //   *rc.borrow_mut() = Some(component);
+  // });
 }
 
 #[wasm_bindgen]
@@ -40,32 +39,32 @@ impl Interface {
 
     let event_name: EventName = e.parse().unwrap();
 
-    ROOT_COMPONENT.with(|rc| {
-      let mut root_component = rc.replace(None).expect("app_state is missing");
+    // ROOT_COMPONENT.with(|rc| {
+    //   let mut root_component = rc.replace(None).expect("ROOT_COMPONENT is missing");
 
-      let mut token = root_component.render();
-      let matched_token = match_token(&mut token, &path);
+    //   let mut token = root_component.render();
+    //   let matched_token = match_token(&mut token, &path);
 
-      if let Some(HtmlToken::DomElement(ref mut d)) = matched_token {
-        d.event_handlers.remove(&event_name).map(|handler| {
-          // handler: &std::boxed::Box<dyn std::ops::FnBox(jsx_types::Event)>
-          js_fns::log("found handler");
-          handler(Event {});
-        });
-      }
+    //   if let Some(HtmlToken::DomElement(ref mut d)) = matched_token {
+    //     d.event_handlers.get_mut(&event_name).map(|handler| {
+    //       // handler: &std::boxed::Box<dyn std::ops::FnBox(jsx_types::Event)>
+    //       js_fns::log("found handler");
+    //       handler(Event {});
+    //     });
+    //   }
 
-      rc.replace(Some(root_component));
-    });
+    //   rc.replace(Some(root_component));
+    // });
   }
 }
 
-fn match_token<'a>(html_token: &'a mut HtmlToken, path: &[usize]) -> Option<&'a mut HtmlToken> {
+fn match_token<'a>(html_token: &'a mut HtmlToken<'a>, path: &[usize]) -> Option<&'a mut HtmlToken<'a>> {
   match path.split_first() {
     None => Some(html_token),
     Some((child_index, rest)) => {
       match html_token {
         HtmlToken::DomElement(d) => {
-          // TODO figure out why the fuck filtering is not working!!!! WAAH
+          // TODO figure out why filtering is not working!!!! WAAH
           match d.children.get_mut(*child_index) {
             Some(child) => match_token(child, rest),
             None => None,
