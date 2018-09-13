ff0ce30eb4f212e24ce40d5631038024163e622d
Robert Balicki <robert.balicki@gmail.com>
2018-09-12 22:57:48 -0400

    further along with new web sys things

--- src/js_fns.rs
+++ src/js_fns.rs
@@ -1,6 +1,8 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
+use web_sys::MouseEvent;
+
 use super::Interface;
 
 #[wasm_bindgen]
@@ -11,10 +13,16 @@ extern {
   #[wasm_bindgen(js_namespace = console, js_name=log)]
   pub fn log(msg: &str);
 
+  #[wasm_bindgen(js_namespace = console, js_name=error)]
+  pub fn log_error(msg: &str);
+
   #[wasm_bindgen(module = "./lib")]
   pub fn initialize(id: &str, interface: Interface);
 
-  // N.B. this should not be needed to be called
-  // #[wasm_bindgen(module = "./lib")]
-  // pub fn render(inner_html: &str);
+  #[wasm_bindgen(js_name = setInterval)]
+  pub fn set_interval(cb: &Closure<FnMut()>, delay: u32) -> f64;
+
+  pub type HTMLElement;
+  #[wasm_bindgen(method, setter)]
+  pub fn set_onclick(this: &HTMLElement, cb: &Closure<FnMut(MouseEvent)>);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,133 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 extern crate jsx_types;
-use jsx_types::{*, events};
+extern crate web_sys;
+extern crate js_sys;
+
+use jsx_types::{*, events, bare};
+use bare::BareHtmlToken;
 
 use std::cell::RefCell;
+
+use web_sys::{Element, Document, Window, MouseEvent, HtmlElement, EventTarget, Event, Node};
 
 pub mod js_fns;
 
 type ShouldUpdate = bool;
+type ComponentAlt = Box<dyn for<'a> Component<'a, ()>>;
 
 thread_local! {
-  static ROOT_COMPONENT: RefCell<Option<Box<dyn for<'a> Component<'a, ()>>>> = RefCell::new(None);
+  // DO NOT USE
+  static ROOT_COMPONENT: RefCell<Option<ComponentAlt>> = RefCell::new(None);
   static LAST_RENDERED_TOKEN: RefCell<Option<jsx_types::bare::BareHtmlToken>> = RefCell::new(None);
+
+  // NEW
+  static ROOT_ELEMENT: RefCell<Option<HtmlElement>> = RefCell::new(None);
 }
 
-pub fn mount(div_id: &str, component: Box<dyn for<'a> Component<'a, ()>>) {
-  js_fns::initialize(div_id, Interface {});
+fn get_document() -> Document { Window::document().unwrap() }
 
-  ROOT_COMPONENT.with(|rc| {
-    *rc.borrow_mut() = Some(component);
+fn mount_to_element(el: &Element, mut component: ComponentAlt) {
+  let token: BareHtmlToken = component.render(()).as_bare_token();
+  el.set_inner_html(&token.as_inner_html());
+}
+
+fn store_root_element(el: Element) {
+  let html_el = unsafe {
+    std::mem::transmute::<Element, HtmlElement>(el)
+  };
+  js_fns::log("initializating root c");
+  ROOT_ELEMENT.with(|rc| {
+    js_fns::log(&format!("settin"));
+    rc.replace(Some(html_el));
   });
 }
+
+fn find_child_index(parent: &Node, child: &Node) -> usize {
+  let node_list = parent.child_nodes();
+  for i in 0..node_list.length() {
+    if let Some(node) = node_list.get(i) {
+      if node.is_same_node(Some(&child)) {
+        return i as usize;
+      }
+    }
+  }
+  // LOL STFU no... should we panic here?
+  0
+}
+
+fn get_path_from(root_element: &HtmlElement, target_element: &HtmlElement) -> Vec<usize> {
+  let root_node: &Node = unsafe {
+    std::mem::transmute::<&HtmlElement, &Node>(root_element)
+  };
+  let target_node: &Node = unsafe {
+    std::mem::transmute::<&HtmlElement, &Node>(target_element)
+  };
+
+  let mut current_node = target_node.parent_node().unwrap();
+  let mut path = vec![find_child_index(&current_node, target_node)];
+  while let Some(parent) = current_node.parent_node() {
+    path.push(find_child_index(&parent, &current_node));
+    if parent.is_same_node(Some(&root_node)) {
+      break;
+    }
+    current_node = parent;
+  }
+
+  path
+}
+
+fn attach_listeners(el: &Element) {
+  let html_el = unsafe {
+    std::mem::transmute::<&Element, &js_fns::HTMLElement>(el)
+  };
+
+  let on_click_cb = Closure::new(|e: MouseEvent| {
+    js_fns::log(&format!("on click handler {:?}", e.client_y()));
+
+    let event = unsafe {
+      std::mem::transmute::<MouseEvent, Event>(e)
+    };
+    if let Some(t) = event.target() {
+      let target_html_el: HtmlElement = unsafe {
+        std::mem::transmute::<EventTarget, HtmlElement>(t)
+      };
+      ROOT_ELEMENT.with(|rc| {
+        let root_element_opt = rc.replace(None);
+        if let Some(root_element) = root_element_opt {
+          let path = get_path_from(&root_element, &target_html_el);
+          js_fns::log(&format!("got root element bra with path {:?}", path));
+          rc.replace(Some(root_element));
+        }
+      })
+    }
+  });
+  html_el.set_onclick(&on_click_cb);
+  on_click_cb.forget();
+}
+
+pub fn mount(div_id: &str, component: ComponentAlt) {
+  let doc: Document = get_document();
+  let el_opt: Option<Element> = doc.get_element_by_id(div_id);
+  
+  // let el2 = js_fns::get_html_element_by_id(&doc, div_id);
+
+  match el_opt {
+    Some(el) => {
+      mount_to_element(&el, component);
+      attach_listeners(&el);
+      store_root_element(el);
+    },
+    None => {
+      js_fns::log_error(&format!("Smithy failed to mount! Element with id {} not found.", div_id));
+    },
+  };
+}
+
+#[wasm_bindgen]
+pub fn trigger_click() {
+  js_fns::log("clickadee-doo");
+} 
 
 #[wasm_bindgen]
 pub struct Interface {}
@@ -55,7 +162,7 @@ impl Interface {
     serde_json::to_string(&diff).unwrap() 
   }
 
-  fn render_as_bare_token(&self) -> jsx_types::bare::BareHtmlToken {
+  fn render_as_bare_token(&self) -> BareHtmlToken {
     ROOT_COMPONENT.with(|rc| {
       let mut component = rc.replace(None).expect("ROOT_COMPONENT is missing");
       let (ret, component) = {
